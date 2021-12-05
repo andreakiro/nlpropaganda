c1933aab3515ec78e7cb85ead840971663b31a6e
giacomocamposampiero <giacomo.camposampiero@hotmail.com>
2021-12-05 23:44:02 +0100

    File updated

--- src/si/si_metric.py
+++ src/si/si_metric.py
@@ -5,8 +5,8 @@ import torch
 from allennlp.training.metrics.metric import Metric
 
 class SpanIdenficationMetric(Metric):
-
-    def __init__(self, ) -> None:
+    
+    def __init__(self) -> None:
         self.t_cardinality = 0
         self.s_cardinality = 0
         self.s_sum = 0
@@ -19,13 +19,15 @@ class SpanIdenficationMetric(Metric):
         self.ct = 0
 
     def __call__(self, prop_spans: torch.Tensor, gold_spans: torch.Tensor, mask: Optional[torch.BoolTensor]):
-        self.t_cardinality += gold_spans.size(dim=1)
-        self.s_cardinality += prop_spans.size(dim=1)
-        for predicted_spans, true_spans in zip(prop_spans[article, :, :], gold_spans[article, :, :]):
-            for combination in itertools.product(predicted_spans, true_spans):
+        for article in range(prop_spans.size(dim=0)):
+            # merge overlapping predicted spans in the same article
+            prop_spans[article] = merge_intervals(prop_spans[article])
+            for combination in itertools.product(prop_spans[article, :, :], gold_spans[article, :, :]):
                 tspan = combination[0]
                 sspan = combination[1]
+                # comput C(s,t,|s|) and add it to the partial sum
                 self.s_sum += c_function(sspan, tspan, sspan[1]-sspan[0])
+                # comput C(s,t,|t|) and add it to the partial sum
                 self.t_sum += c_function(sspan, tspan, tspan[1]-tspan[0])
 
     def get_metric(self, reset: bool = False):
@@ -38,22 +40,55 @@ class SpanIdenficationMetric(Metric):
         return (2 * precision * recall) / (precision + recall) if precision + recall > 0 else 0
 
     def c_function(s, t, h):
+        """
+        Compute C(s,t,h)
+        :param s: predicted span 
+        :param t: gold span
+        :param h: normalizing factor
+        :return: value of the function for the given parameters
+        """
+        intersection = intersect(s, t)
+        return intersection/h if intersection > 0 else 0    
+
+    
+    def intersect(s, t):
+        """
+        Intersect two spans.
+        :param s: first span
+        :param t: second span
+        :return: # of intersecting words between the spans, if < 0 represent the distance between spans
+        """
         start = max(s[0], t[0])
         end = min(s[1], t[1])
-        return (end - start + 1)/h if end >= start else 0    
+        return end - start + 1
 
     def merge_intervals(prop_spans):
-        stack = list()
-        indices = prop_spans[:, :, 0].sort()[1] 
-        a_sorted = a[torch.arange(a.size(0)).unsqueeze(1), indices] 
-        logger.info(f"predictions: {token.text}")
-        logger.info(f"sorted predictions: {token.text}")
-        torch.stack(sorted(prop_spans, key=lambda a: a[0]))        
+        """
+        Merge overlapping spans in the given span tensor.
+        :param prop_spans: spans to be merged
+        :return: tensor contained only non-overlapping spans
+        """
+        last = 0
+        # sort the predicted spans by start index
+        prop_spans[prop_spans[:, 0].sort()[1]]
+        # debug
+        logger.info(f"please check me!")
+        logger.info(f"predictions: {prop_spans}")
+        logger.info(f"sorted predictions: {prop_spans_sorted}")
+        # for each span in the sorted tensor, check for intersection with rightmost span analyzed
+        for index in range(prop_spans_sorted.size(dim=0)):
+            if((not last) or (intersect(prop_spans_sorted[index,:], prop_spans_sorted[last,:]) < -1)):
+                # add the span to the stack if it's empty or if the current span 
+                # does not intersect with the last span in the stack or has not distance 1 from it
+                last += 1
+            else:
+                # current stack intersect with rightmost span analyzed
+                # update last element right index with the max between the two intersecting
+                prop_spans_sorted[last,1] = max(prop_spans_sorted[last,1], prop_spans_sorted[index,1])
+                # remove the other from the tensor
+                logger.info(f"please check me!")
+                logger.info(f"size before deletion: {prop_spans_sorted.size(dim=0)}")
+                prop_spans_sorted = prop_spans_sorted[torch.arange(prop_spans_sorted.size(dim=0))!=index]
+                logger.info(f"size after deletion: {prop_spans_sorted.size(dim=0)}")
 
-a = torch.randn(2, 5, 2)
-indices = a[:, :, 0].sort()[1] 
-a_sorted = a[torch.arange(a.size(0)).unsqueeze(1), indices] 
-print(a, "\n\n") 
-a = torch.stack(sorted(a, key=lambda a: a[:,:,0]))        
-print("mio",a,"\n\n")
-print(a_sorted)
+        return prop_spans_sorted
