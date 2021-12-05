56d94814a119f80cac6cd26c6ed90f1f471c1352
Andrea Pinto <andreakiro.pinto@gmail.com>
2021-12-05 19:46:45 +0100

    TC dataset reader almost finished

--- src/tc/tc_reader.py
+++ src/tc/tc_reader.py
@@ -1,6 +1,5 @@
 from typing import Dict, Iterable, List, Tuple
 import logging
-import string
 import os
 
 from overrides import overrides
@@ -9,9 +8,11 @@ import torch
 
 from allennlp.data.dataset_readers.dataset_utils.span_utils import enumerate_spans
 from allennlp.data import DatasetReader, Instance
-from allennlp.data.fields import Field, ListField, TextField, SpanField
+from allennlp.data.fields import Field, ListField, TextField, SpanField, SequenceLabelField
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 from allennlp.data.tokenizers import Tokenizer, SpacyTokenizer
+
+from src.si.si_model import SpanIdentifier
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +23,7 @@ class TechniqueClassificationReader(DatasetReader):
     Either for training or for testing purposes. Returns a 
     `Dataset` where the `Instances` have at least two fields
     and a third one if we are dealing with a training session.
-    
+
     fields["articles-content"]: `TextField` always.
         The entire article content in a TextField.
     fields["si-spans"]: `ListField[SpanField] always.
@@ -49,7 +50,7 @@ class TechniqueClassificationReader(DatasetReader):
     """
     def __init__(
         self,
-        si_model: torch.nn,
+        si_model: SpanIdentifier,
         max_span_width: int,
         articles_dir_path: str,
         labels_file_path: str = None,
@@ -74,61 +75,79 @@ class TechniqueClassificationReader(DatasetReader):
 
     @overrides
     def _read(self, file_path: str) -> Iterable[Instance]:
-        # Note: file_path is folder_to_articles_path 
-        logger.info(f"Preparing Dataset for {self._task}")
-        logger.info(f"Searching for data in {self.articles_dir_path}")
-
-        instances: list[Dict[str, Field]] = []
-
-        logger.info(f"Loading articles content from {self._articles_dir_path}")
-        with os.scandir(self._articles_dir_path) as articles:
+        articles_dir = file_path
+        with os.scandir(articles_dir) as articles:
             for article in articles:
-                assert article.name.endswith(".txt"), "Articles must be under txt format."
+                content: str = None
+                gold_label_spans: List[Tuple[str, int, int]] = None
+
                 with open(article, 'r') as file:
                     content = file.read()
-                    instances.append({"article-content": content})
 
-        # add every si-spans predicted from si-model ?
-
-        if self._task == "training":
-            logger.info(f"Loading labels from {self._labels_file_path}")
-            with open(self._labels_file_path) as lines:
-                {}
-
-        # fields: Dict[str, Field] = {}
-        # fields["articles-content"] = _get_articles_instances(self)
-        # fields["si-spans"] = _get_si_spans_instances(self)
-
-        # if self._task == "training":
-        #     fields["gold-spans"] = _get_gold_spans_instances(self)
-
-        # return Iterable[Instance[fields]]
-        return None
-
-    @overrides
-    def text_to_instance(self, *inputs) -> Instance:
-        return super().text_to_instance(*inputs)
-
-    def _get_article_instances(self) -> Field:
-        return Field
-
-    def _get_si_spans_instances(self) -> Field:
-        return Field
-
-    def _get_gold_spans_instances(self) -> Field:
-        return None
-
+                if "test" not in articles_dir:
+                    gold_label_spans = []
+                    with open(article.name[:-3] + 'task-tc.labels', 'r') as lines:
+                        for line in lines:
+                            _, label, span_start, span_end = line.strip().split("\t")
+                            gold_label_spans.append((label, span_start, span_end))
+                
+                # add assertions
+                yield self.text_to_instance(content, gold_label_spans)
     
-# 1) Lire tous les articles
-# 2) Lire tous les labels files
-# 3) Créer une méthode filter()
-#   filter(si-model, enumerated_spans) -> filtered_spans
-# 4) Créer les instances i.e. :
-#   Si training: 
-#       TextField (article)
-#       ListField[SpanField]
-#       ListField[LabeledSequenceField] (spans labelisés)
-#           LabelSequenceField := Tuple[SpanField, int]
-#   Si test:
-#       TextField (article)
-#       ListField[SpanField]
+    @overrides
+    def text_to_instance(
+        self, content: str, 
+        gold_label_spans: List[Tuple[str, int, int]] = None
+    ) -> Instance:
+        fields: Dict[str, Field] = {}
+
+         # Create the TextField for the article
+        tokens = self._tokenizer.tokenize(content)
+        article_field = TextField(tokens, self._token_indexer)
+        fields["content"] = article_field
+
+        # Extract article spans and add them to instance
+        all_spans = enumerate_spans(tokens, max_span_width = self._max_span_width)
+        pruned_spans = all_spans  #TODO Prune spans by using heuristics
+        si_spans = self._si_model(article_field, pruned_spans)
+
+        # Add si-spans to our field dict
+        fields["si-spans"] = ListField([SpanField(start, end, article_field) for start, end in si_spans])
+
+        if gold_label_spans is not None:
+            # Add gold-spans to our field dict
+            labels = self._get_labels(si_spans, gold_label_spans)
+            fields["gold-spans"] = SequenceLabelField(labels, fields["si-spans"])
+
+        return Instance(fields)
+
+    def _get_labels(
+        self,
+        si_spans: torch.IntTensor,
+        gold_label_spans: torch.IntTensor,
+    ) -> torch.IntTensor:
+        """
+        # Parameters
+        si_spans: `torch.IntTensor` required.
+            Batch of SpanFields (?) on which to perform training or prediction.
+        gold_spans: `torch.IntTensor` optional.
+            Batch of SpanFields corresponding labels when training the model.
+        
+        # Returns
+        labels: `torch.IntTensor` always
+            Labels of span s for every span s in si_spans appearing in gold_spans
+            Label "no propaganda" for every span s in si_spans not appearing in gold_spans
+        """
+        labels: List[int] = []
+        
+        for span in si_spans:
+            labelled = False
+            for tup in gold_label_spans:
+                if span[0] == tup[1] and span[1] == tup[2]: # smarter ? achtung mapping int->str
+                    labels.append(tup[0])
+                    labelled = True
+                    break
+            if not labelled:
+                labels.append(0)
+        
+        return labels
