35e6f2acdaf27b16b6589c8150e0a1aee4cd4b1f
Antoine Basseto <antoine.basseto@gmail.com>
2021-12-06 17:47:50 +0100

    Bug fixing on si reader

--- src/si/si_reader.py
+++ src/si/si_reader.py
@@ -17,14 +17,12 @@ logger = logging.getLogger(__name__)
 class SpanIdentificationReader(DatasetReader):
     def __init__(
             self,
-            data_dir_path: str,
             max_span_width: int,
             tokenizer: Tokenizer = SpacyTokenizer(),
             token_indexers: Dict[str, TokenIndexer] = {"tokens": SingleIdTokenIndexer()},
             **kwargs
         ):
             super().__init__(**kwargs)
-            self._data_dir_path = data_dir_path
             self._max_span_width = max_span_width
             self.tokenizer = tokenizer
             self.token_indexers = token_indexers
@@ -35,21 +33,26 @@ class SpanIdentificationReader(DatasetReader):
 
         with os.scandir(articles_dir) as articles:
             for article in articles:
+                if not article.name.endswith(".txt"):
+                    continue
                 content: str = None
                 gold_spans: List[Tuple[int, int]] = None
+
+                logger.info(f"Reading {article.name[:-3]}")
 
                 with open(article, 'r') as file:
                     content = file.read()
 
                 if "test" not in articles_dir:
                     gold_spans = []
-                    with open(article.name[:-3] + 'task-si.labels', 'r') as lines:
+                    with open(os.path.join(articles_dir, "labels", article.name[:-3] + 'task-si.labels'), 'r') as lines:
                         for line in lines:
                             _, span_start, span_end = line.strip().split("\t")
-                            gold_spans.append((span_start, span_end))
+                            gold_spans.append((int(span_start), int(span_end)))
                 
                 # add assertions
                 yield self.text_to_instance(content, gold_spans)
+        logger.info(f"Finished reading {articles_dir}")
     
     @overrides
     def text_to_instance(self, text: str, gold_spans: List[Tuple[int, int]] = None) -> Instance:
@@ -63,57 +66,73 @@ class SpanIdentificationReader(DatasetReader):
         # Add gold spans to instance
         if gold_spans is not None:
             gold_spans_in_token_space = self._map_to_token_space(text, gold_spans, tokens)
-            list_gold_span_fields: List[SpanField] = [SpanField(start, 
-                                                                end, 
-                                                                article_field) 
-                                                                for start, end in gold_spans_in_token_space]
-            fields["gold_spans"] = ListField(list_gold_span_fields)
+            list_gold_span_fields: List[SpanField] = [SpanField(start, end, article_field) for start, end in gold_spans_in_token_space]
+            fields["gold_spans"] = ListField(list_gold_span_fields) if len(list_gold_span_fields) != 0 else ListField.empty_field
 
         # Extract article spans and add them to instance
         spans = enumerate_spans(tokens, max_span_width=self._max_span_width)
-        list_span_fields: List[SpanField] = [SpanField(start, 
-                                                       end, # enumerate_spans returns spans with inclusive boundaries
-                                                       article_field) 
-                                                       for start, end in spans]
+        list_span_fields: List[SpanField] = [SpanField(start, end, article_field) for start, end in spans]
         
         #TODO Prune spans, by using heuristics
         fields["spans"] = ListField(list_span_fields)
 
         return Instance(fields)
 
-    def _map_to_token_space(self, text, gold_spans, tokens):
+    def _map_to_token_space(self, text: str, gold_spans, tokens):
         # Mapping gold spans to the token space
         gold_spans_starts = [start for start, _ in gold_spans]
-        gold_spans_tokens = [self.tokenizer.tokenize(text[start:end]) for start, end in gold_spans]
-        gold_spans_in_token_space = []
+        mapping = {}
+
         text_index = 0
+        if text_index in gold_spans_starts:
+            mapping[text_index] = 0
+        
         for token_index, token in enumerate(tokens):
-            if text_index in gold_spans_starts:
-                logger.info(f"START OF PROPAGANDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-                # assert gold_spans_tokens[len(gold_spans_in_token_space)] in tokens[token_index:token_index+len(gold_spans_tokens[len(gold_spans_in_token_space)])], "Mismatch between character and token space"
-                gold_spans_in_token_space.append(token_index)
             
-            logger.info(f"text index before: {text_index}")
-            logger.info(f"token index: {token_index}")
-            logger.info(f"token: {token.text}")
-            text_index += len(token.text)
-            logger.info(f"text index after: {text_index}")
-            logger.info(f"text after this: {text[text_index: text_index+10]}")
+            tmp = text_index
+            while text_index < tmp + len(token.text):
+                text_index += 1
+                if text_index in gold_spans_starts:
+                    mapping[text_index] = token_index
 
             while text_index < len(text) and text[text_index] in string.whitespace:
                 text_index += 1
-            logger.info(f"text index after whitespace: {text_index}")
-        
-        gold_spans_in_token_space = [(start, start + len(tokens) - 1) for start, tokens in zip(gold_spans_in_token_space, gold_spans_tokens)]
+                if text_index in gold_spans_starts:
+                    mapping[text_index] = token_index
 
-        # gold_spans_in_token_space = [[x for x in range(len(tokens)) if [token.text for token in tokens[x:x+len(span_tokens)]] == [token.text for token in span_tokens]] for span_tokens in gold_spans_tokens]
-        # Debug
-        logger.info(f"text\n{text}")
-        logger.info(f"tokens\n{tokens}")
-        logger.info(f"gold spans text\n{[text[start:end] for start, end in gold_spans]}")
-        logger.info(f"gold spans tokens\n{gold_spans_tokens}")
-        logger.info(f"gold spans in token space\n{gold_spans_in_token_space}")
-        logger.info(f"gold spans in token space in tokens\n{[tokens[start:end+1] for start, end in gold_spans_in_token_space]}")
-        # assert np.all([len(starts) == 1 for starts in gold_spans_in_token_space]), "Failed conversion of span in token space"
-        # SpanField's bounds are inclusive
+        gold_spans_tokens = [self.tokenizer.tokenize(text[start:end+1]) for start, end in gold_spans]
+        logger.info(f"gold spans tokens\n\n{gold_spans_tokens}\n")
+
+        gold_spans_in_token_space = [mapping[start] for start, _ in gold_spans]
+        gold_spans_in_token_space = [(start_token, len(self.tokenizer.tokenize(text[span[0]:span[1]]))) for start_token, span in zip(gold_spans_in_token_space, gold_spans)]
+
+        logger.info(f"gold spans in token space in tokens\n\n{[tokens[start:end+1] for start, end in gold_spans_in_token_space]}\n")
+        return gold_spans_in_token_space
+
+    def _map_to_token_space_2(self, text: str, gold_spans, tokens):
+        # Mapping gold spans to the token space
+        gold_spans_flattened = [bound for span in gold_spans for bound in span]
+        mapping = {}
+
+        text_index = 0
+        if text_index in gold_spans_flattened:
+            mapping[text_index] = 0
+        
+        for token_index, token in enumerate(tokens):
+            
+            tmp = text_index
+            while text_index < tmp + len(token.text):
+                text_index += 1
+                if text_index in gold_spans_flattened:
+                    mapping[text_index] = token_index
+
+            while text_index < len(text) and text[text_index] in string.whitespace:
+                text_index += 1
+                if text_index in gold_spans_flattened:
+                    mapping[text_index] = token_index
+
+        gold_spans_tokens = [self.tokenizer.tokenize(text[start:end+1]) for start, end in gold_spans]
+        logger.info(f"gold spans tokens\n\n{gold_spans_tokens}\n")
+        gold_spans_in_token_space = [(mapping[start], mapping[end]) for start, end in gold_spans]
+        logger.info(f"gold spans in token space in tokens\n\n{[tokens[start:end+1] for start, end in gold_spans_in_token_space]}\n")
         return gold_spans_in_token_space
